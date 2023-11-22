24325acf88ef200328e9e420eefade7bcca2bbb1
Debangshu <debangshu182002@gmail.com>
2023-11-22 20:10:42 +0530

    search implemented

---
+++ src/app/search/page.tsx
@@ -0,0 +1,35 @@
+import ProductCard from "@/components/ProductCard"
+import prisma from "@/lib/db/prisma"
+
+interface SearchPageProps {
+    searchParams : {query : string}
+}
+
+export default async function SearchPage({ searchParams : {query}} : SearchPageProps) {
+    const products = await prisma.product.findMany({
+        where : {
+            OR : [
+                {name : {contains : query, mode : "insensitive"}},
+                {description : {contains : query, mode : "insensitive"}},
+            ]
+        },
+        orderBy: {id : "desc"}
+    })
+
+    if(products.length === 0){
+        return <div className="text-center">No Products Found</div>
+    }
+
+    return (
+        <div>
+            <h1 className="text-lg font-bold mb-6 ">Results for <span className="text-accent capitalize">{query}</span></h1>
+
+            <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4 ">
+            {products.map(product => (
+                <ProductCard product={product} key={product.id}/>
+            ))}
+        </div>
+        </div>
+    )
+
+}
